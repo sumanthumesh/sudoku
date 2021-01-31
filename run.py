393e7052d2159969e082a3477cbd45196ac6b68c
Appu <sumanth689@gmail.com>
2021-01-31 13:58:48 +0530

    modified run.py for reference

--- run.py
+++ run.py
@@ -1,35 +1,35 @@
-from sudoku import sudoku
+from solver import Solver
 import sys
 import numpy as np
+         
+original_stdout = sys.stdout
+file = open("run_solver.log","w")
+sys.stdout = file
 
-DEBUG = False
-
-if(DEBUG == True):
-    original_stdout = sys.stdout
-    file = open("run_sudoku.log","w")
-    sys.stdout = file                
-S = sudoku()
+S = Solver()
 #print(S.group(5,4))
             
-sample = np.array([[1,7,2,5,4,9,6,8,3],
-                    [6,4,5,8,7,3,2,1,9],
-                    [3,8,9,2,6,1,7,4,5],
-                    [4,9,6,3,2,7,8,5,1],
-                    [8,1,3,4,5,6,9,7,2],
-                    [2,5,7,1,9,8,4,3,6],
-                    [9,6,4,7,1,5,3,2,8],
-                    [7,3,1,6,8,2,5,9,4],
-                    [5,2,8,9,3,4,1,6,7]])
+sample = [[5,3,0,0,7,0,0,0,0],
+                    [6,0,0,1,9,5,0,0,0],
+                    [0,9,8,0,0,0,0,6,0],
+                    [8,0,0,0,6,0,0,0,3],
+                    [4,0,0,8,0,3,0,0,1],
+                    [7,0,0,0,2,0,0,0,6],
+                    [0,6,0,0,0,0,2,8,0],
+                    [0,0,0,4,1,9,0,0,5],
+                    [0,0,0,0,8,0,0,7,9]]
 
-#S.F = sample
-
+S.debug(False)
+S.F = np.array(sample)
+S.solve()
 #print(S.isInGroup(1, 3))
 #print(S.isLegal(9,4,6))
-S.debug(False)
-S.generate()
 #print("Finished")
-if(DEBUG == True):
-    sys.stdout = original_stdout
-    file.close()
     
+
+
+
+
+sys.stdout = original_stdout
+
 S.display()
